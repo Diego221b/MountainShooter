6d9058e216bf832342ae9415852a6a701978a07b
Diego221b <diego.souza221b@gmail.com>
2025-11-11 19:48:38 -0300

    Creating main loop, connecting Menu and Game

---
+++ code/Game.py
@@ -0,0 +1,22 @@
+import pygame
+
+from code.Menu import Menu
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(600, 480))
+
+    def run(self, ):
+        print("Setup Start")
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+
+            #Check for all events
+            #for event in pygame.event.get():
+            #    if event.type == pygame.QUIT:
+            #        print("Quitting...")
+            #        pygame.quit()
+            #        quit()  #end pygame
