2ee200442f6e2f710af82cfbca80805a13a46e66
Diego221b <diego.souza221b@gmail.com>
2025-11-11 20:03:24 -0300

    add assets

--- code/Menu.py
+++ code/Menu.py
@@ -1,6 +1,10 @@
+import pygame
+
 class Menu:
     def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load('./asset/MenuBg.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
         pass
