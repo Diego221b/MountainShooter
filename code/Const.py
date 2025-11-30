8c171a98b57b413ec12173a54efdeb3e6ea7ef71
Diego221b <diego.souza221b@gmail.com>
2025-11-30 16:13:55 -0300

    HUD and Score

--- code/Const.py
+++ code/Const.py
@@ -1,12 +1,52 @@
 # C
 import pygame
 
-COLOR_ORANGE = (255, 128, 0)
-COLOR_WHITE = (255, 255, 255)
-COLOR_YELLOW = (255, 255, 0)
+C_ORANGE = (255, 128, 0)
+C_WHITE = (255, 255, 255)
+C_YELLOW = (255, 255, 0)
+C_GREEN = (0, 128, 0)
+C_CYAN = (0, 128, 128)
 
 #E
 EVENT_ENEMY =pygame.USEREVENT + 1
+ENTITY_DAMAGE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Player1': 1,
+    'Player1Shot': 25,
+    'Player2': 1,
+    'Player2Shot': 25,
+    'Enemy1': 1,
+    'Enemy1Shot': 20,
+    'Enemy2': 1,
+    'Enemy2Shot': 80,
+}
+
+ENTITY_SCORE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Player1': 0,
+    'Player1Shot': 0,
+    'Player2': 0,
+    'Player2Shot': 0,
+    'Enemy1': 100,
+    'Enemy1Shot': 0,
+    'Enemy2': 125,
+    'Enemy2Shot': 0,
+}
+
+
+
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 0.90,
@@ -16,10 +56,10 @@ ENTITY_SPEED = {
     'Level1Bg5': 5,
     'Level1Bg6': 6,
     'Player1': 3,
-    'Player1Shot': 3,
+    'Player1Shot': 5,
     'Player2': 3,
-    'Player2Shot': 3,
-    'Enemy1': 1,
+    'Player2Shot': 5,
+    'Enemy1': 2,
     'Enemy1Shot': 5,
     'Enemy2': 1,
     'Enemy2Shot': 2,
@@ -60,6 +100,7 @@ MENU_OPTION = ('NEW GAME 1P',
 #P
 PLAYER_KEY_UP = {'Player1': pygame.K_UP,
                  'Player2': pygame.K_w}
+
 PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
                    'Player2': pygame.K_s}
 PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
